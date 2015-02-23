ca5b829e1fa8f4fa0cf256738ee2eeea515c2531
anton <anton@pc>
2015-02-23 23:41:09 +0300

    Добавлены ресурсы для TFileTreeView

--- LazRes/make.sh
+++ LazRes/make.sh
@@ -1,2 +1,5 @@
 #bin/sh
-./lazres resource.lrs TemplateLib.txt TemplateUnit.txt
+./lazres resource.lrs TemplateLib.txt TemplateUnit.txt 
+cp resource.lrs ..
+./lazres treeview.lrs ../images/folder.png ../images/text-x-pascal.png
+cp treeview.lrs ..
